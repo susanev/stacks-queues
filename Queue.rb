cff81806f526548473a2e4bf3a5193f6395032ec
susan <susan.ra.evans@gmail.com>
2016-08-31 11:50:21 -0700

    wrote constructor and methods for Stack and Queue classes

--- Queue.rb
+++ Queue.rb
@@ -1,19 +1,25 @@
 class Queue
   def initialize
+    @store = Array.new
   end
   
   def enqueue(element)
+    @store << element
   end
  
   def dequeue
+    @store.delete(front)
   end
 
   def front
+    return @store.first
   end
  
   def size
+    return @store.length
   end
 
   def empty?
+    return @store.length == 0
   end
 end
--- Stack.rb
+++ Stack.rb
@@ -4,17 +4,22 @@ class Stack
   end
   
   def push(element)
+    @store << element
   end
  
   def pop
+    return @store.delete(top)
   end
 
   def top
+    return @store.last
   end
  
   def size
+    return @store.length
   end
 
   def empty?
+    return @store.length == 0
   end
 end
